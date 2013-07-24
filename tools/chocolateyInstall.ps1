5780f84aac606d996ca8ad28073e2b5b702c1b9f
Brendan Forster <me@brendanforster.com>
2013-07-24 13:43:27 -0500

    Version bump

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'beyondcompare' 'exe' '/SP- /VERYSILENT /NORESTART' 'http://www.scootersoftware.com/BCompare-3.3.7.15876.exe'
+﻿Install-ChocolateyPackage 'beyondcompare' 'exe' '/SP- /VERYSILENT /NORESTART' 'http://www.scootersoftware.com/BCompare-3.3.8.16340.exe'
