1dd71234f63ee944e4313fbdd65c40670afa1958
David Gardiner <david@gardiner.net.au>
2014-06-27 16:51:08 +0930

    Update to v3.3.11.18371

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'beyondcompare' 'exe' '/SP- /VERYSILENT /NORESTART' 'http://www.scootersoftware.com/BCompare-3.3.10.17762.exe'
+﻿Install-ChocolateyPackage 'beyondcompare' 'exe' '/SP- /VERYSILENT /NORESTART' 'http://www.scootersoftware.com/BCompare-3.3.11.18371.exe'
