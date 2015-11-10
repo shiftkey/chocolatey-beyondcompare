d74c92c1af29ad71f7151f885c5dbecb34f0ea87
sippi90 <sippi-@mail.com>
2015-11-10 18:46:49 +0100

    Update chocolateyInstall.ps1
    autodetect new versions

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,16 +1,19 @@
 $LCID = (Get-Culture).LCID
 $german = @(3079,1031,5127,4103,2055)
 $french = @(2060,11276,3084,9228,12300,1036,5132,13324,6156,14348,10252,4108,7180)
+$source = 'http://www.scootersoftware.com/download.php'
+$HTML = Invoke-WebRequest $source
+$version = (($HTML.ParsedHtml.links|  Where-Object {$_.pathname -like "*BCompare*"} | Select-Object pathname -First 1).pathname).replace('BCompare-','').trimend('.exe')
 if ($LCID -in $german)
 {
-    $url = 'http://www.scootersoftware.com/BCompare-de-4.0.7.19761.exe'
+    $url = 'http://www.scootersoftware.com/BCompare-de-'+$version+'.exe'
 }
 elseif ($LCID -in $french)
 {
-    $url = 'http://www.scootersoftware.com/BCompare-fr-4.0.7.19761.exe'
+    $url = 'http://www.scootersoftware.com/BCompare-fr-'+$version+'.exe'
 }
 else
 {
-    $url = 'http://www.scootersoftware.com/BCompare-4.0.7.19761.exe'
+    $url = 'http://www.scootersoftware.com/BCompare-'+$version+'.exe'
 }
 Install-ChocolateyPackage 'beyondcompare' 'exe' '/SP- /VERYSILENT /NORESTART' $url
