aa4229242590a0f77d6a72b3ef490f7f8548f5b7
Brendan Forster <brendan@github.com>
2015-12-20 12:34:08 +1030

    updated package assets

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-$LCID = (Get-Culture).LCID
+﻿$LCID = (Get-Culture).LCID
 $german = @(3079,1031,5127,4103,2055)
 $french = @(2060,11276,3084,9228,12300,1036,5132,13324,6156,14348,10252,4108,7180)
-$version = '4.1.2.20720'
+$version = '4.1.3.20814'
 if ($LCID -in $german)
 {
     $url = 'http://www.scootersoftware.com/BCompare-de-'+$version+'.exe'
